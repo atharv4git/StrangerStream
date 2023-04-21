b0d0340b3517882c2930e7fe0edc3c2e43326afc
atharv4git <atharv4study@gmail.com>
2023-04-21 08:29:58 +0530

    video feed added, start/stop buttons added

--- src/page1.py
+++ src/page1.py
@@ -1,8 +1,45 @@
-from flask import Flask
+import cv2
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
+# capture frames from the webcam
+camera = cv2.VideoCapture(0)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!<br>hehe"
+run_camera = True
+
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n'
+                   + frame + b'\r\n')
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/shutdown')
+def shutdown():
+    camera.release()
+    cv2.destroyAllWindows()
+    return 'Shutting down...'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
